365b42a896c4092165ae3e5f7f25f0f11118d136
John Williams <willi762@uwm.edu>
2016-02-26 17:08:33 -0600

    Partially completed simulator/ChronoDriver

--- ChronoTimer/ChronoTimerProject/src/ChronoDriver.java
+++ ChronoTimer/ChronoTimerProject/src/ChronoDriver.java
@@ -1,10 +1,41 @@
 import java.io.File;
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import chronoTimerMain.simulator.Simulator;
 
 public class ChronoDriver {
-	public static void main (String [] args){
+	public static void main (String [] args) throws IOException{
 		Simulator consoleSim = new Simulator();
-		Simulator fileSim = new Simulator(new File("PLACEHOLDER"));
+		Simulator fileSim=new Simulator(new File("PLACEHOLDER"));
+		Timer listenTimer=new Timer();
+		listenTimer.scheduleAtFixedRate(new ListenTask(fileSim),(long) 0, (long) 10);
+		
+		
+		
 	};
+	//TODO
+	public void parseCommand(String command){
+		
+	}
+	static class ListenTask extends TimerTask {
+        Simulator sim;
+		public ListenTask(Simulator sim){
+        	this.sim=sim;
+        }
+		@Override
+		public void run() {
+			listen(sim);
+		}
+		public void listen(Simulator sim){
+			if(sim.listen()){
+				String event=sim.getEvent();
+				switch(event){
+				case "Tog 1":
+				}//etc, translate event strings to chrono timer commands
+			}
+		}
+    }
+	
 }
--- ChronoTimer/ChronoTimerProject/src/chronoTimerMain/simulator/Simulator.java
+++ ChronoTimer/ChronoTimerProject/src/chronoTimerMain/simulator/Simulator.java
@@ -1,11 +1,85 @@
 package chronoTimerMain.simulator;
 
-import java.io.File;
+import java.io.*;
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class Simulator {
-	public Simulator(){};
-	public Simulator(File file){};
-		
-	public void captureInputLoop(){};
+	private boolean eventWaiting;
+	private boolean fileinput;
+	private long initialTime;
+	private ConcurrentLinkedQueue<String> events=new ConcurrentLinkedQueue<String>();
+	private ConcurrentLinkedQueue<Long> eventTimes;
+	
+	public Simulator(){
+		fileinput=false;
+	}
+	
+	public Simulator(File file) throws IOException{
+		eventTimes=new ConcurrentLinkedQueue<Long>();
+		captureInputLoop(file);
+		fileinput=true;
+	}
+	
+	//start the simulation
+	public void start(){
+		if(fileinput){
+			Timer t=new Timer();
+			t.schedule(new RunTask(),eventTimes.remove()-initialTime);
+		}
+	}
+	
+	public void changeEventwaiting(){
+		eventWaiting=!eventWaiting;
+	}
+	
+	public Long getNextTime(){
+		return(eventTimes.remove());
+	}
+	
+	public void captureInputLoop(File file) throws NumberFormatException, IOException{
+		FileInputStream fis = new FileInputStream(file);
+		//Construct BufferedReader from InputStreamReader
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+		String[] temp=null;
+		String[] temp2=null;
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			//parse time and add to queue
+			temp=line.split("/t");
+			temp2=temp[0].split(".");
+			long t=Time.valueOf(temp[0]).getTime();
+			t+=Long.parseLong(temp2[1]);
+			eventTimes.add(t);
+			//add command to queue to be parsed by driver
+			events.add(temp[1]);
+			}
+		initialTime=eventTimes.peek();
+		br.close();
+	};
+	//TODO also switch with above
 	public void readFile(File file) {};
+	
+	public boolean listen(){//is there an event?
+		return(eventWaiting);
+	}
+	
+	public String getEvent(){
+		if(listen()){
+			return(events.remove());
+		}else{
+			return("No command waiting");
+		}
+	}
+}
+//TODO
+class RunTask extends TimerTask {
+	
+	@Override
+	public void run() {
+		Simulator.changeEventwaiting();
+		
+	}
 }
