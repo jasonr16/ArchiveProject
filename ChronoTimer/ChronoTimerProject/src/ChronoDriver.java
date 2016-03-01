e1ee6ef8e52a5a3675553dd36d9c17e23927eb4f
John Williams <willi762@uwm.edu>
2016-03-01 09:57:57 -0600

    added io to determine what time of simulator input to use and get file path

--- ChronoTimer/ChronoTimerProject/src/ChronoDriver.java
+++ ChronoTimer/ChronoTimerProject/src/ChronoDriver.java
@@ -1,5 +1,4 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -7,15 +6,36 @@ import chronoTimerMain.simulator.Simulator;
 
 public class ChronoDriver {
 	public static void main (String [] args) throws IOException{
-		Simulator consoleSim = new Simulator();
-		Simulator fileSim=new Simulator(new File(""));
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String input="";
+		boolean actionSuccess=false;
+		Simulator Sim;
+		while(!actionSuccess){
+			System.out.print("File or Console input<C or F> :");
+			input=br.readLine().trim().toUpperCase();
+			if(input.equals("C")||input.equals("F")) actionSuccess=true;
+		}
+		switch(input){
+		case "C":Sim = new Simulator();
+				 break;
+		case "F":
+			while(!actionSuccess){
+				try{
+					System.out.print("Input File path:");
+					input=br.readLine();
+					Sim=new Simulator(new File(input));
+					}catch(IOException e){
+						System.out.println("Incorrect file path");
+					}
+			}
+			default:Sim=new Simulator();
+		}
 		Timer listenTimer=new Timer();
-		listenTimer.scheduleAtFixedRate(new ListenTask(fileSim),(long) 0, (long) 10);
-		fileSim.start();
+		listenTimer.scheduleAtFixedRate(new ListenTask(Sim),(long) 0, (long) 10);
+		Sim.start();
 		
 		
 	};
-	//TODO
 	
 	static class ListenTask extends TimerTask {
         Simulator sim;
@@ -25,10 +45,7 @@ public class ChronoDriver {
 		@Override
 		public void run() {
 			Command currentCommand;
-			//System.out.print("0");
 			if(sim.listen()){
-				
-				
 				currentCommand=new Command(sim.getEvent());
 				currentCommand.execute();
 			}
