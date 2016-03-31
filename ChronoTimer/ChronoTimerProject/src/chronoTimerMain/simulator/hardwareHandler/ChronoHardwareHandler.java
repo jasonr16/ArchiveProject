2cf4d8181b9125853810676ded13e24870c1cf2d
Jason R <jbregall@uwm.edu>
2016-03-31 10:46:42 -0500

    moved SingleEvent Hardware class to its own class file

--- ChronoTimer/ChronoTimerProject/src/chronoTimerMain/simulator/hardwareHandler/ChronoHardwareHandler.java
+++ ChronoTimer/ChronoTimerProject/src/chronoTimerMain/simulator/hardwareHandler/ChronoHardwareHandler.java
@@ -23,41 +23,6 @@ import junit.framework.TestCase;
  */
 public class ChronoHardwareHandler {
 	
-	/**
-	 * SingleEvent class is used to store all events entered into the system.
-	 * It will eventually be implemented to export to XML.
-	 * @author Jason
-	 *
-	 */
-	protected class SingleEvent {
-		private String timeStamp;
-		private String command;
-		private String[] args;
-		protected  SingleEvent(String timeStamp, String command, String[] args) {
-			this.setTimeStamp(timeStamp);
-			this.setCommand(command);
-			this.setArgs(args);
-		}
-		protected String getTimeStamp() {
-			return timeStamp;
-		}
-		protected void setTimeStamp(String timeStamp) {
-			this.timeStamp = timeStamp;
-		}
-		protected String getCommand() {
-			return command;
-		}
-		protected  void setCommand(String command) {
-			this.command = command;
-		}
-		protected  String[] getArgs() {
-			return args;
-		}
-		protected void setArgs(String[] args) {
-			this.args = args;
-		}
-	}
-	
 	protected Timer time = new Timer();
 	protected Sensor[] sensors = new Sensor[13];//no sensor stored in index 0. 12 max
 	protected boolean[] isEnabledSensor = new boolean[13];
